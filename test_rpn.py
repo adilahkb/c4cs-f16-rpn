61fc8845ae5449eb6b238ffe64349cec050d44dc
Adilah Ku <kuadilah@umich.edu>
2016-10-15 18:17:10 -0400

    edit

--- test_rpn.py
+++ test_rpn.py
@@ -18,6 +18,6 @@ class TestBasics(unittest.TestCase):
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
-	#def test_exp(self):
-		#result = rpn.calculate("2 3 ^")
-		#self.assertEqual(8, result)
+	def test_exp(self):
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result)
